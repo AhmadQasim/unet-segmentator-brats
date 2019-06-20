d552605e2a639e4b181c5683b15b451f51e0a1cf
ahmad <ahmad.qasim@glosus.com>
2019-06-20 16:39:09 +0200

    adding the synthetic dataset support to dice and other improvements

--- examples/tumor_segmentation.py
+++ examples/tumor_segmentation.py
@@ -15,14 +15,26 @@ os.environ['CUDA_VISIBLE_DEVICES'] = '2'
 
 DATA_DIR = '/home/qasima/segmentation_models.pytorch/data/'
 RESULT_DIR = '/home/qasima/segmentation_models.pytorch/results/'
-MODEL_NAME = 'model_epochs_10_pure_resnet34_multimodal_DICE_1_pixel'
-EPOCHS_NUM = 10
+MODEL_NAME = 'model_epochs_30_multi_modal_pure_resnet34_dice_1_pixel_syn'
+EPOCHS_NUM = 15
+SYNTHETIC_RATIO = 0.5
+TEST_RATIO = 0.1
 
 x_dir = dict()
+x_dir_syn = dict()
+x_dir_test = dict()
 x_dir['t1ce'] = os.path.join(DATA_DIR, 'train_t1ce_img_full')
 x_dir['t1'] = os.path.join(DATA_DIR, 'train_t1_img_full')
 x_dir['t2'] = os.path.join(DATA_DIR, 'train_t2_img_full')
 y_dir = os.path.join(DATA_DIR, 'train_label_full')
+x_dir_syn['t1ce'] = os.path.join(DATA_DIR, 'train_t1ce_img_full_syn')
+x_dir_syn['t1'] = os.path.join(DATA_DIR, 'train_t1_img_full_syn')
+x_dir_syn['t2'] = os.path.join(DATA_DIR, 'train_t2_img_full_syn')
+y_dir_syn = os.path.join(DATA_DIR, 'train_label_full_syn')
+x_dir_test['t1ce'] = os.path.join(DATA_DIR, 'train_t1ce_img_full_syn')
+x_dir_test['t1'] = os.path.join(DATA_DIR, 'train_t1_img_full_syn')
+x_dir_test['t2'] = os.path.join(DATA_DIR, 'train_t2_img_full_syn')
+y_dir_test = os.path.join(DATA_DIR, 'train_label_full_syn')
 
 
 # this function fuses the predicted mask classes into one image
@@ -66,22 +78,25 @@ class Dataset(BaseDataset):
         image = np.stack([image_t1ce, image_t1, image_t2], axis=-1).astype('float32')
         mask = cv2.imread(self.masks_fps[i], cv2.IMREAD_GRAYSCALE)
 
+        if image.shape[0] == 256:
+            mask = cv2.copyMakeBorder(mask, 8, 8, 8, 8, cv2.BORDER_CONSTANT, (0, 0, 0))
+
         # extract certain classes from mask
         masks = [(mask == v) for v in self.class_values]
-        mask = np.stack(masks, axis=-1).astype('float32')
+        mask_stacked = np.stack(masks, axis=-1).astype('float32')
 
         # apply augmentations
         if self.augmentation:
-            sample = self.augmentation(image=image, mask=mask)
-            image, mask = sample['image'], sample['mask']
+            sample = self.augmentation(image=image, mask=mask_stacked)
+            image, mask_stacked = sample['image'], sample['mask']
 
         # modification: adding a singleton dimension so that the image can be processed
-        mask = np.swapaxes(mask, 1, 2)
-        mask = np.swapaxes(mask, 0, 1)
+        mask_stacked = np.swapaxes(mask_stacked, 1, 2)
+        mask_stacked = np.swapaxes(mask_stacked, 0, 1)
         image = np.swapaxes(image, 0, 2)
         image = np.swapaxes(image, 1, 2)
 
-        return image, mask
+        return image, mask_stacked
 
     def __len__(self):
         return len(self.ids)
@@ -105,6 +120,7 @@ CLASSES = ['t_2', 't_1', 't_3']
 ACTIVATION = 'sigmoid'
 
 model = torch.load('./' + MODEL_NAME)
+
 '''
 # create segmentation model with pretrained encoder
 model = smp.Unet(
@@ -114,15 +130,24 @@ model = smp.Unet(
     activation=ACTIVATION,
 )
 '''
-
-preprocessing_fn = smp.encoders.get_preprocessing_fn(ENCODER, ENCODER_WEIGHTS)
-
 full_dataset = Dataset(
     x_dir,
     y_dir,
     classes=CLASSES,
     augmentation=get_training_augmentation(),
 )
+
+full_dataset_syn = Dataset(
+    x_dir_syn,
+    y_dir_syn,
+    classes=CLASSES,
+    augmentation=get_training_augmentation(),
+)
+
+synthetic_size = len(full_dataset_syn) - int(len(full_dataset_syn)*(1-SYNTHETIC_RATIO))
+
+full_dataset_syn = torch.utils.data.Subset(full_dataset_syn, np.arange(synthetic_size))
+full_dataset = torch.utils.data.ConcatDataset((full_dataset, full_dataset_syn))
 
 train_size = int(0.9 * len(full_dataset))
 valid_size = len(full_dataset) - train_size
@@ -134,7 +159,7 @@ metrics = [
 ]
 
 optimizer = torch.optim.Adam([
-    {'params': model.decoder.parameters(), 'lr': 1e-5},
+    {'params': model.decoder.parameters(), 'lr': 1e-4},
     {'params': model.encoder.parameters(), 'lr': 1e-6},
 ])
 
@@ -157,7 +182,7 @@ valid_epoch = smp.utils.train.ValidEpoch(
     verbose=True,
 )
 
-
+'''
 max_score = 0
 
 for i in range(0, EPOCHS_NUM):
@@ -179,12 +204,19 @@ for i in range(0, EPOCHS_NUM):
         torch.save(model, './' + MODEL_NAME)
         print('Model saved!')
 
-    if i == 25:
+    if i == 10:
         optimizer.param_groups[0]['lr'] = 1e-5
         print('Decrease decoder learning rate to 1e-5!')
 '''
 # load best saved checkpoint
 best_model = torch.load('./' + MODEL_NAME)
+
+full_dataset_test = Dataset(
+    x_dir_test,
+    y_dir_test,
+    classes=CLASSES,
+    augmentation=get_training_augmentation(),
+)
 
 # evaluate model on test set
 test_epoch = smp.utils.train.ValidEpoch(
@@ -193,13 +225,20 @@ test_epoch = smp.utils.train.ValidEpoch(
     metrics=metrics,
     device=DEVICE,
 )
-train_dataset, valid_dataset = torch.utils.data.random_split(full_dataset,
-                                                                 [train_size, valid_size])
-valid_loader = DataLoader(valid_dataset, batch_size=3, shuffle=False, num_workers=2)
-logs = test_epoch.run(valid_loader)
+
+test_size = int(len(full_dataset_test)*TEST_RATIO)
+remaining_size = len(full_dataset_test) - test_size
+
+train_dataset, test_dataset = torch.utils.data.random_split(full_dataset_test,
+                                                                 [remaining_size, test_size])
+test_loader = DataLoader(test_dataset, batch_size=3, shuffle=False, num_workers=2)
+logs = test_epoch.run(test_loader)
 
 best_model = torch.load('./' + MODEL_NAME)
 model_result_dir = os.path.join(RESULT_DIR, MODEL_NAME)
+
+if not os.path.exists(model_result_dir):
+    os.mkdir(model_result_dir)
 
 # save some prediction mask samples
 for i in range(10):
@@ -219,4 +258,3 @@ for i in range(10):
     imsave(model_result_dir + '/real_image_t2_{}.png'.format(i), image[2, :, :])
     imsave(model_result_dir + '/real_mask_{}.png'.format(i), gt_img)
     imsave(model_result_dir + '/predicted_mask_{}.png'.format(i), pr_img)
-'''
